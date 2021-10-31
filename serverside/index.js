11eb838633cba707855e972b9167b2b1336e1bd2
Shivam Singh <shivamsingh4458@gmail.com>
2021-10-31 22:20:14 +0530

    adding index file

--- serverside/index.js
+++ serverside/index.js
@@ -1,25 +1,51 @@
 require("dotenv").config();
-
 const express = require("express");
-
-const connect = require("./src/configs/db");
-
-const userController = require("./src/controller/user.controller")
-
-
-const PORT = process.env.PORT || 5000;
-
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-app.use(express.json());
-
-app.use("/", userController);
-
-app.listen(PORT, async () => {
-
-    await connect();
-    console.log(`Listening on port ${PORT}`);
-
-})
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
 
+const port = process.env.PORT || 5000
+
+const connect = require("./src/config/db");
+
+const postcontroller = require("./src/controllers/posts.controlers");
+
+app.use(logger("dev"));
+
+const dbUrl = connect.dbUrl;
+
+var options = {
+  keepAlive: 1,
+  connectTimeoutMS: 30000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(dbUrl, options, (err) => {
+
+  if (err) {
+    console.log("ERROR", err);
+  }
+
+});
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/", postcontroller);
+
+app.listen(port, function () {
+
+  console.log("Listening on post " + port);
+
+});
+
+module.exports = app;

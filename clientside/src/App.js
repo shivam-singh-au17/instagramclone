cb4b0270b73f952ea0d1b4a72e78a2894fb676b6
SHIVAM SINGH <76985787+shivam-singh-au17@users.noreply.github.com>
2021-11-13 08:58:57 +0530

    Update App.js file

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -1,24 +1,12 @@
-import Home from "./pages/Home";
-import Messenger from "./pages/messenger/Messenger";
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import Routes from "./Routes/Routes";
 
 function App() {
-	const user = [];
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<Home />} />
-
-				{/* <Route
-					path="/login"
-					element={user ? < Navigate to="/" /> : "Login"}
-				>
-					{user ? <Navigate to="/" /> : "Login"}
-				</Route> */}
-				<Route path="/messenger" element={<Messenger />} />
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Routes />
+    </>
+  );
 }
 
 export default App;
